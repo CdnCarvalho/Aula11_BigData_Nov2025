7e65c0384b14b6864b2c14e75fd80e017ec3ffeb
CdnCarvalho <claudenirfer@gmail.com>
2026-01-06 19:14:32 -0300

    random para gerar números decimais

--- aula11/exemplo01.py
+++ aula11/exemplo01.py
@@ -4,4 +4,11 @@ import random
 # random.randint -> Números Inteiros
 n = random.randint(1, 10)
 m = random.randint(1, 10)
-print(n, m)
+# print(n, m)
+
+# -------------------------------------------------
+# Gerar números decimais
+n_decimal = random.uniform(1, 10)
+numero_decimal = round(n_decimal, 1) # Arredonda
+print(f'{n_decimal:.2f}') # Formata para duas casas
+print(numero_decimal)
